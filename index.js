a4ee38b79e87f5a660053a653f93b58c1f397f5c
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-08-23 11:14:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function superbowlWin(record){
+    let object=record.find(function(object){
+        return object.result==='W';
+    });
+    if (typeof object === 'undefined'){
+        return undefined;
+    }
+    else{
+        return object.year;
+    }
+}
